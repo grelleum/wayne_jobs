7d4e934da66206bf7b9b489057f55bbdd8e47697
Greg Mueller <greg@grelleum.com>
2024-12-23 21:28:13 -0500

    Add diffsync adapters.

--- jobs/sync_locations.py
+++ jobs/sync_locations.py
@@ -3,37 +3,16 @@
 2024 Greg Mueller.
 """
 
-
 import csv
 from io import StringIO
-from typing import Generator, List, Optional
+from typing import Optional
 
 from django.urls import reverse
-
+from diffsync import Adapter
 from nautobot.apps.jobs import FileVar, register_jobs
-from nautobot.dcim.models import Location, LocationType
-
-
-
-
-# import requests
-# from diffsync import Adapter
-# from diffsync.enum import DiffSyncFlags
-# from diffsync.exceptions import ObjectNotFound
-# from django.contrib.contenttypes.models import ContentType
-# from django.templatetags.static import static
-# from nautobot.dcim.models import Device, DeviceType, Interface, Location, LocationType, Manufacturer, Platform
-# from nautobot.extras.choices import SecretsGroupAccessTypeChoices, SecretsGroupSecretTypeChoices
-# from nautobot.extras.jobs import ObjectVar, StringVar
-# from nautobot.extras.models import ExternalIntegration, Role, Status
-# from nautobot.extras.secrets.exceptions import SecretError
-# from nautobot.ipam.models import IPAddress, Namespace, Prefix
-# from nautobot.tenancy.models import Tenant
-
+from nautobot.dcim.models import Location
 from nautobot_ssot.contrib import NautobotAdapter, NautobotModel
 from nautobot_ssot.jobs.base import DataMapping, DataSource
-# from nautobot_ssot.tests.contrib_base_classes import ContentTypeDict
-
 
 
 name = "Wayne Enterprises: Custom Jobs"
@@ -129,42 +108,40 @@ class NautobotLocal(NautobotAdapter):
     top_level = ["location"]
 
 
+class NautobotRemote(Adapter):
+    """DiffSync adapter class for loading data from a CSV File."""
 
-class LocationsCSVDataSource(DataSource):
-    """Job that imports Location data from a CSV file."""
+    location = LocationModel
+    top_level = ["location"]
 
-    source_file = FileVar(description="CSV file containing Locations data")
+    def __init__(self, *args, source_file, job=None, **kwargs):
+        """Instantiate this class."""
+        super().__init__(*args, **kwargs)
+        self.job = job
+        self.source_file = source_file
 
+    def load(self):
+        """Load data from the provided."""
+        csv_records = self.get_csv_records(self.source_file)
+        location_records = self.get_all_location_records(csv_records)
+        self.load_locations(location_records)
 
-    class Meta:
-        """Metaclass attributes of LocationsCSVDataSource."""
+    def load_locations(self, location_records):
+        """Load Locations data from the remote Nautobot instance."""
+        for location_record in location_records:
+            location = self.location(**location_record)
+            self.add(location)
+            self.job.logger.debug(
+                f"Loaded {location} Location from remote Nautobot instance"
+            )
 
-        name = "Import locations from CSV file."
-        description = "Job that keeps the Locations table up to date."
-        data_source = "Locations CSV File (remote)"
-
-    @classmethod
-    def data_mappings(cls):
-        """Map remote source data to local Nautobot models."""
-        return (
-            DataMapping("Location (remote)", None, "Location (local)", reverse("dcim:location_list")),
-        )
-
-    def run(self, *args, source_file, **kwargs):
-        self.logger.info(f"{repr(args)=}")
-        self.logger.info(f"{repr(kwargs)=}")
-        records = self.get_csv_data(source_file)
-        self.logger.info(f"{repr(records)=}")
-        source_data = self.get_source_data(records)
-        self.logger.info(f"{repr(source_data)=}")
-
-    def get_csv_data(self, source_file):
+    def get_csv_records(self, source_file):
         text = source_file.read().decode("utf-8")
         csv_data = csv.DictReader(StringIO(text))
         records = list(csv_data)
         return records
 
-    def get_source_data(self, records):
+    def get_all_location_records(self, records):
         return [
             *self.get_states(records),
             *self.get_cities(records),
@@ -182,7 +159,7 @@ class LocationsCSVDataSource(DataSource):
             }
             for state_name in states
         ]
-        return state_records        
+        return state_records
 
     def get_cities(self, records):
         cities = set((r["city"], r["state"]) for r in records)
@@ -197,7 +174,7 @@ class LocationsCSVDataSource(DataSource):
             }
             for city_name, state_name in cities
         ]
-        return city_records        
+        return city_records
 
     def get_location_sites(self, records):
         site_records = []
@@ -221,72 +198,48 @@ class LocationsCSVDataSource(DataSource):
             }
             site_records.append(record)
 
-        return site_records        
+        return site_records
 
 
-    # def run(  # pylint: disable=too-many-arguments, arguments-differ
-    #     self,
-    #     dryrun,
-    #     memory_profiling,
-    #     source,
-    #     source_url,
-    #     source_token,
-    #     *args,
-    #     **kwargs,
-    # ):
-    #     """Run sync."""
-    #     self.dryrun = dryrun
-    #     self.memory_profiling = memory_profiling
-    #     try:
-    #         if source:
-    #             self.logger.info(f"Using external integration '{source}'")
-    #             self.source_url = source.remote_url
-    #             if not source.secrets_group:
-    #                 self.logger.error(
-    #                     "%s is missing a SecretsGroup. You must specify a SecretsGroup to synchronize with this Nautobot instance.",
-    #                     source,
-    #                 )
-    #                 raise MissingSecretsGroupException(message="Missing SecretsGroup on specified ExternalIntegration.")
-    #             secrets_group = source.secrets_group
-    #             self.source_token = secrets_group.get_secret_value(
-    #                 access_type=SecretsGroupAccessTypeChoices.TYPE_HTTP,
-    #                 secret_type=SecretsGroupSecretTypeChoices.TYPE_TOKEN,
-    #             )
-    #         else:
-    #             self.source_url = source_url
-    #             self.source_token = source_token
-    #     except SecretError as error:
-    #         self.logger.error("Error setting up job: %s", error)
-    #         raise
+class LocationsCSVDataSource(DataSource):
+    """Job that imports Location data from a CSV file."""
 
-    #     super().run(dryrun, memory_profiling, *args, **kwargs)
+    source_file = FileVar(description="CSV file containing Locations data")
 
-    # def load_source_adapter(self):
-    #     """Method to instantiate and load the SOURCE adapter into `self.source_adapter`."""
-    #     self.source_adapter = NautobotRemote(url=self.source_url, token=self.source_token, job=self)
-    #     self.source_adapter.load()
+    class Meta:
+        """Metaclass attributes of LocationsCSVDataSource."""
 
-    # def load_target_adapter(self):
-    #     """Method to instantiate and load the TARGET adapter into `self.target_adapter`."""
-    #     self.target_adapter = NautobotLocal(job=self, sync=self.sync)
-    #     self.target_adapter.load()
+        name = "Import locations from CSV file."
+        description = "Job that keeps the Locations table up to date."
+        data_source = "Locations CSV File (remote)"
 
-    # def lookup_object(self, model_name, unique_id):
-    #     """Look up a Nautobot object based on the DiffSync model name and unique ID."""
-    #     if model_name == "prefix":
-    #         try:
-    #             return Prefix.objects.get(
-    #                 prefix=unique_id.split("__")[0], tenant__name=unique_id.split("__")[1] or None
-    #             )
-    #         except Prefix.DoesNotExist:
-    #             pass
-    #     elif model_name == "tenant":
-    #         try:
-    #             return Tenant.objects.get(name=unique_id)
-    #         except Tenant.DoesNotExist:
-    #             pass
-    #     return None
+    @classmethod
+    def data_mappings(cls):
+        """Map remote source data to local Nautobot models."""
+        return (
+            DataMapping(
+                "Location (remote)",
+                None,
+                "Location (local)",
+                reverse("dcim:location_list"),
+            ),
+        )
 
+    def run(self, *args, dryrun, memory_profiling, source_file, **kwargs):
+        self.source_file = source_file
+        self.dryrun = dryrun
+        self.memory_profiling = memory_profiling
+        super().run(dryrun, memory_profiling, *args, **kwargs)
+
+    def load_source_adapter(self):
+        """Method to instantiate and load the SOURCE adapter into `self.source_adapter`."""
+        self.source_adapter = NautobotRemote(source_file=self.source_file, job=self)
+        self.source_adapter.load()
+
+    def load_target_adapter(self):
+        """Method to instantiate and load the TARGET adapter into `self.target_adapter`."""
+        self.target_adapter = NautobotLocal(job=self, sync=self.sync)
+        self.target_adapter.load()
 
 
 jobs = [LocationsCSVDataSource]
